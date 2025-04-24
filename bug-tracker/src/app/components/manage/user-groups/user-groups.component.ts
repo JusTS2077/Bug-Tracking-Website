5ee17b2e0d80ee7da613b6287fd6a6164a63546a
JusTS2077 <justin.shijoy@gmail.com>
2025-04-24 12:18:38 +0530

    added user group tab

---
+++ bug-tracker/src/app/components/manage/user-groups/user-groups.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-groups',
+  imports: [],
+  templateUrl: './user-groups.component.html',
+  styleUrl: './user-groups.component.css'
+})
+export class UserGroupsComponent {
+
+}
